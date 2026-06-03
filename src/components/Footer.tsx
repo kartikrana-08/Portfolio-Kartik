931f0622bf4e007c34ff9ba87c03489b63a13fd2
kartikrana-08 <263571957+kartikrana-08@users.noreply.github.com>
2026-06-03 12:59:15 +0530

    Fix: remove unused useEffect imports causing TS build errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'preact/hooks'
+import { useState } from 'preact/hooks'
 import { theme } from '../theme'
 import { useScrollReveal } from '../hooks/useScrollReveal'
 import { useIsMobile } from '../hooks/useIsMobile'
